f1b2f89b92593b49137377021bb9293a2a0a86ec
Shant Agopian <penguintesting641@gmail.com>
2024-06-10 10:26:47 -0700

    Code Add
    Added PoC Code and Config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# mac1=STA
+# mac2=AP
+sta_target = 'XX:XX:XX:XX:XX:XX' # change this
+ap_target = 'YY:YY:YY:YY:YY:YY' # change this
+# spray=how many frames to send
+spray = 500
+# check interval is for airodump timer to see if bssid is still up 
+check_interval = 60
